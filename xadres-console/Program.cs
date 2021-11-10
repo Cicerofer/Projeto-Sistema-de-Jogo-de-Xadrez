bb4f7c6a48cd9fe2a890434410abc438ac87ad9f
Cicerofer <ciceroferreira2010@hotmail.com>
2021-11-10 18:39:04 -0300

    iniciando projeto

---
+++ xadres-console/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tabuleiro;
+
+namespace xadres_console
+{
+    class Program
+    {
+      static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição:" + P );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xadres-console/Tabuleiro/Cor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadres_console.Tabuleiro
+{
+    enum Cor
+    {Branca,
+     Preta,
+     Amarela,
+     Vermelha,
+     Verde,
+     Laranja
+
+
+
+    }
+}
